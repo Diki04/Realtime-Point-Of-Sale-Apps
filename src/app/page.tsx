0427ece11fe83798bf775c071f5b2e0e90e2265b
Diki04 <sinyofamily152520@gmail.com>
2025-07-17 21:22:11 +0700

    feat: darkmode_toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 
 export default function Home() {
   return(
     <div>
       <Input placeholder="Type something..." />
       <Button>Click Me</Button>
+      <DarkModeToggle />
     </div>
   )
 }
